4cb6cd90a0197b6db33ffaf555bf543642b2ade5
Yuri Fukuda <airinterface@gmail.com>
2024-05-30 13:35:45 -0400

    updating workflow

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const organizationName = "MicroEdgeCerts";
-const projectName = "documentation";
+const projectName = "microedgecerts.github.io";
 
 const config: Config = {
   title: 'Micro Edge Certs',
